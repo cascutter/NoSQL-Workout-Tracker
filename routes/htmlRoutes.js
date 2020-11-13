4175f6a8108a8720e0d9e20589ad52412dd6f9c6
cascutter <cascutter13@gmail.com>
2020-11-13 12:42:33 -0600

    Fixed typos and syntax errors

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -7,7 +7,7 @@ module.exports = function(app) {
     });
 
     app.get("/exercise", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/exercise/html"));
+        res.sendFile(path.join(__dirname, "../public/exercise.html"));
     });
 
     app.get("/stats", function(req, res) {
