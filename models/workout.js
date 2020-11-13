2b6c8294cdcbac18b156f92a55f95a7028134cf6
cascutter <cascutter13@gmail.com>
2020-11-13 11:29:45 -0600

    Models for index and workout created

---
+++ models/workout.js
@@ -0,0 +1,56 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+
+const workoutSchema = new Schema({
+            day: {
+                type: Date,
+                default: () => new Date()
+            },
+            exercises: [{
+                    type: {
+                        type: String,
+                        trim: true,
+                        required: "Exercise Type"
+                    },
+                    name: {
+                        type: String,
+                        trim: true,
+                        required: "Exercise Name"
+                    },
+                    duration: {
+                        type: Number,
+                        required: "Exercise Duration"
+                    },
+                    weight: {
+                        type: Number
+                    },
+                    reps: {
+                        type: Number
+                    },
+                    sets: {
+                        type: Number
+                    },
+                    distance: {
+                        type: Number
+                    }
+                ]
+            },
+            // Includes virtual properties
+            {
+                toJSON: {
+                    virtuals: true
+                }
+            }
+        );
+        // Adds dynamically created property to schema
+        workoutSchema.virtual("titalDuration").get(function() {
+            // Gives array of exercises as just the sum of their durations
+            return this.exercises.reduce((total, exercise) => {
+                return total + exercise.duration;
+            }, 0);
+        });
+
+        const Workout = mongoose.model("Workout", workoutSchema);
+
+        module.exports = Workout;
